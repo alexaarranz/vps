c821ee1e1c85105f4473865f55c2a55b47deed1f
alexaarranz <alex_arranz@hotmail.com>
2026-02-10 20:18:01 +0000

    wg-easy

--- traefik/traefik-init.sh
+++ traefik/traefik-init.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Adds the static route to the WireGuard client subnet via wg-easy's IP on vpn-client-net
-# Adjusts 100.64.0.0/16 (WG subnet) and 192.0.2.18 (Traefik's IP address in vpn-client-net) if the WG subnet or traefik's IP on vpn-client-net differs
-ip route add 100.64.0.0/16 via 192.0.2.18 || true   # || true = don't fail container if route already exists
+# Adjusts 100.64.0.0/16 (WG subnet) and 192.0.2.20 (wg-easy's IP address in vpn-client-net) if the WG subnet or traefik's IP on vpn-client-net differs
+ip route add 100.64.0.0/16 via 192.0.2.20 || true   # || true = don't fail container if route already exists
 
 echo "Custom route added: $(ip route show 100.64.0.0/16)"
 
